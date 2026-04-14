f80242e7d586fe4b4659e5a016880e3b5246425b
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-04-14 13:50:50 +0530

    start command changed

--- server/server.js
+++ server/server.js
@@ -14,6 +14,14 @@ connectDB();
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tours", require("./routes/tourRoutes"));
 
+if (process.env.NODE_ENV === "production") {
+  const clientBuildPath = path.join(__dirname, "..", "client", "latest", "dist");
+  app.use(express.static(clientBuildPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
