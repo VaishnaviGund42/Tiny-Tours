895dda76c347f665da63f04531bee45f6687f23f
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-04-18 15:36:55 +0530

    Fix CORS to allow Vercel origin dynamically

--- server/server.js
+++ server/server.js
@@ -6,7 +6,18 @@ require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173", "https://tiny-tours.vercel.app"], methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"] }));
+app.use(cors({ 
+  origin: function (origin, callback) {
+    const allowedOrigins = ["http://localhost:5173", "https://tiny-tours.vercel.app"];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type", "Authorization"] 
+}));
 app.use(express.json());
 
 connectDB();
